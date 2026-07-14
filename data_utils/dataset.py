8e94d5bec01a03339a4278120be79f959cf728ce
Wancca <757948081@qq.com>
2026-07-14 12:49:37 +0800

    Update dataset.py

--- data_utils/dataset.py
+++ data_utils/dataset.py
@@ -1,86 +1,68 @@
-import pickle
+import os
 import torch
+import numpy as np
 from torch.utils import data
 from torch_geometric.data import Batch
-import numpy as np
+from utils.net_utils import pack1D, pack2D
 
 
-import numpy as np
-import sys
-import os
+class GerNA_FastDataset(data.Dataset):
+    def __init__(self, pt_dir):
+        self.pt_dir = pt_dir
+        self.file_list = sorted(
+            [f for f in os.listdir(pt_dir) if f.startswith('sample_') and f.endswith('.pt')],
+            key=lambda x: int(x.split('_')[1].split('.')[0])
+        )
+        self.num_samples = len(self.file_list)
 
-from torch_geometric.data import Data
-from torch.autograd import Variable
-import torch.optim as optim
-import random
-from utils.net_utils import *
-from utils.metrics import *
-
-torch.multiprocessing.set_sharing_strategy('file_system')
-
-
-class GerNA_dataset(data.Dataset):
-    def __init__(self, dataset_path):
-        with open(dataset_path, 'rb') as f:
-            self.data = pickle.load(f)
-        self.RNA_repre, self.Mol_graph, self.RNA_Graph, self.RNA_feats, self.RNA_C4_coors, self.RNA_coors, self.Mol_feats, self.Mol_coors, self.LAS_input, self.Label_list = self.data
-        
     def __len__(self):
-        return len(self.RNA_repre)
+        return self.num_samples
 
     def __getitem__(self, index):
-        return self.RNA_repre[index], self.Mol_graph[index], self.RNA_Graph[index], self.RNA_feats[index], self.RNA_C4_coors[index], self.RNA_coors[index], self.Mol_feats[index], self.Mol_coors[index], self.LAS_input[index], self.Label_list[index]
+        path = os.path.join(self.pt_dir, f"sample_{index}.pt")
+        # 确保 weights_only=False 以加载自定义类
+        return torch.load(path, weights_only=False)
 
 
-def custom_collate_fn(batch):
-    batch_RNA_repre = []
-    batch_seq_mask = []
-    batch_Mol_Graph = []
-    batch_RNA_Graph = []
-    batch_RNA_feats = []
-    batch_RNA_C4_coors = []
-    batch_RNA_coors = []
-    batch_RNA_mask = []
-    batch_Mol_feats = []
-    batch_Mol_coors = []
-    batch_Mol_mask = []
-    batch_Mol_LAS = []
-    batch_label = []
-    
-    for i,item in enumerate(batch):
-        rna_repre,mol_graph,rna_graph,rna_feats,rna_coors_C4,rna_coors,mol_feats,mol_coors,mol_las,label = item
-        
-        batch_RNA_repre.append(rna_repre)
-        batch_Mol_Graph.append(mol_graph)
-        batch_RNA_Graph.append(rna_graph)
-        
-        batch_RNA_feats.append(rna_feats)
-        batch_RNA_C4_coors.append(rna_coors_C4)
-        batch_RNA_coors.append(rna_coors)
+def fast_collate_fn(batch):
 
-        batch_Mol_feats.append(mol_feats)
-        batch_Mol_coors.append(mol_coors)
+    # 1. 提取各组件
+    RNA_repre = [item['RNA_repre'] for item in batch]
+    RNA_feats = [item['RNA_feats'] for item in batch]
+    RNA_C4_coors = [item['RNA_C4_coors'] for item in batch]
+    RNA_coors = [item['RNA_coors'] for item in batch]
+    Mol_feats = [item['Mol_feats'] for item in batch]
+    Mol_coors = [item['Mol_coors'] for item in batch]
+    Mol_LAS = [item['Mol_LAS'] for item in batch]
 
-        batch_Mol_LAS.append(mol_las)
-        batch_label.append(label)
-    
-    batch_seq_mask = torch.tensor( np.array(get_mask(batch_RNA_repre))).float()#.to(device)
-    batch_RNA_repre = torch.tensor( np.array(pack2D(batch_RNA_repre))).float()#.to(device)
-    
-    batch_Mol_Graph = Batch.from_data_list(batch_Mol_Graph)#.to(device)
-    batch_RNA_Graph = Batch.from_data_list(batch_RNA_Graph)#.to(device)
-    
-    batch_RNA_mask = torch.tensor( np.array(get_mask(batch_RNA_feats))).bool()#.to(device)
-    batch_Mol_mask = torch.tensor( np.array(get_mask(batch_Mol_feats))).bool()#.to(device)
-    
-    batch_RNA_feats = torch.tensor( np.array(pack1D(batch_RNA_feats))).long()#.to(device)
-    batch_RNA_C4_coors = torch.tensor( np.array(pack2D(batch_RNA_C4_coors)) ).float()
-    batch_RNA_coors = torch.tensor( np.array(pack2D(batch_RNA_coors))).float()#.to(device)
-    
-    batch_Mol_feats = torch.tensor( np.array(pack1D(batch_Mol_feats))).long()#.to(device)
-    batch_Mol_coors = torch.tensor( np.array(pack2D(batch_Mol_coors))).float()#.to(device)
-    
-    batch_Mol_LAS = torch.tensor( np.array(pack2D(batch_Mol_LAS))).float()#.to(device)
-    batch_label = torch.FloatTensor( np.array(batch_label) )#.to(device)
+    # 【核心修复点】：这些 Mask 长度也不一样，不能 stack，必须用 pack1D 对齐
+    RNA_repre_mask_list = [item['RNA_repre_mask'] for item in batch]
+    RNA_feats_mask_list = [item['RNA_feats_mask'] for item in batch]
+    Mol_coors_mask_list = [item['Mol_coors_mask'] for item in batch]
 
-    return batch_RNA_repre, batch_seq_mask, batch_Mol_Graph, batch_RNA_Graph, batch_RNA_feats, batch_RNA_C4_coors, batch_RNA_coors, batch_RNA_mask, batch_Mol_feats, batch_Mol_coors, batch_Mol_mask, batch_Mol_LAS, batch_label
+   
+    batch_label = torch.cat([item['label'] for item in batch])
+
+    
+    batch_RNA_repre = torch.FloatTensor(np.array(pack2D(RNA_repre)))
+    batch_RNA_feats = torch.LongTensor(np.array(pack1D(RNA_feats)))
+    batch_RNA_C4_coors = torch.FloatTensor(np.array(pack2D(RNA_C4_coors)))
+    batch_RNA_coors = torch.FloatTensor(np.array(pack2D(RNA_coors)))
+    batch_Mol_feats = torch.LongTensor(np.array(pack1D(Mol_feats)))
+    batch_Mol_coors = torch.FloatTensor(np.array(pack2D(Mol_coors)))
+    batch_Mol_LAS = torch.FloatTensor(np.array(pack2D(Mol_LAS)))
+
+    # 对 Mask 进行对齐
+    batch_seq_mask = torch.FloatTensor(np.array(pack1D(RNA_repre_mask_list)))
+    batch_RNA_mask = torch.BoolTensor(np.array(pack1D(RNA_feats_mask_list)))
+    batch_Mol_mask = torch.BoolTensor(np.array(pack1D(Mol_coors_mask_list)))
+
+    # 3. 图数据打包 (PyG 对象)
+    batch_Mol_Graph = Batch.from_data_list([item['Mol_graph'] for item in batch])
+    batch_RNA_Graph = Batch.from_data_list([item['RNA_Graph'] for item in batch])
+
+    # 返回顺序必须严格匹配 train_model.py 的解包顺序 (13个变量)
+    return (batch_RNA_repre, batch_seq_mask, batch_Mol_Graph, batch_RNA_Graph,
+            batch_RNA_feats, batch_RNA_C4_coors, batch_RNA_coors,
+            batch_RNA_mask, batch_Mol_feats, batch_Mol_coors,
+            batch_Mol_mask, batch_Mol_LAS, batch_label)
